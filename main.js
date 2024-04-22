fc22a65c62a8d540fab80bc3ebc329cc0d9b812a
xraffay-dev <xraffay2004@gmail.com>
2024-04-22 23:33:05 +0500

    Adding initial files

---
+++ main.js
@@ -0,0 +1,15 @@
+let rockButton = document.querySelector("#rock");
+let paperButton = document.querySelector("#paper");
+let scissorsButton = document.querySelector("#scissors");
+
+rockButton.onclick = () => {
+    console.log("Rock button clicked");
+};
+
+paperButton.onclick = () => {
+    console.log("Paper button clicked");
+};
+
+scissorsButton.onclick = () => {
+    console.log("Scissors button clicked");
+};
